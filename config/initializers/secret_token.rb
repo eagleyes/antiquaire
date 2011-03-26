d922858417d0b1486e8d6d439115b13b201fd517
Pierrette Samson <eagleyes@antiquaire-laptop.(none)>
2011-03-25 22:32:05 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antiquaire::Application.config.secret_token = '7dfa766534ad6de2fe50eac65ec80b9737616a15dac173898bf00a06402b9c61b402692db2ba24bc06bf9f23b02fd24a289fa3a3f14e5acf13a23a815116fb13'
