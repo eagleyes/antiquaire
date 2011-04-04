cb7b289d558fec19af99c5b26e6b14f20b5dcd26
eagleyes <eagleyes@ubuntu-laptop.(none)>
2011-04-04 19:35:58 -0400

    removed description from products table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-      #translate :title, :text 
-      attr_accessor :description , :category , :country , :material, :style  
+      translates :name, :description 
+      attr_accessor :category , :country , :material, :style  
       #attr_accessor :purchase_date , :qty_in_stock , :qty_in_stock_adj, :height, :width , :length , photo
       #
 
@@ -8,19 +8,4 @@ class Product < ActiveRecord::Base
       #			:length => { :maximum => 100 }
 	
       validates :description, :presence => true		 
-
-      def name
-	puts I18n.locale.class
-	if I18n.locale.to_s == "fr-CA" 
-	puts "francais"
-	  return name_fr
-	else
-	puts "anglais"
-	  return name_en
-	end
-
-      end	 
-
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110401171155) do
+ActiveRecord::Schema.define(:version => 20110401225205) do
+
+  create_table "product_translations", :force => true do |t|
+    t.integer  "product_id"
+    t.string   "locale"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "product_translations", ["product_id"], :name => "index_product_translations_on_product_id"
 
   create_table "products", :force => true do |t|
-    t.text     "description_fr"
-    t.text     "description_en"
     t.integer  "category_id"
     t.date     "purchase_date"
     t.integer  "manufacture_year"
